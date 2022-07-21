1688abd64c3e43973dab8d86a7a82f1539ae0f39
eoeefosa <eoeefosa@gmail.com>
2022-07-21 17:42:47 +0100

    modified today _recipe_list_view.dart

--- lib/components/today_recipe_list_view.dart
+++ lib/components/today_recipe_list_view.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+// Import the barrel files, component.dart and models.dart, so you can use data
+// models and UI components.
 import 'components.dart';
 import 'package:fooderlich/models/models.dart';
 
 class TodayRecipeListView extends StatelessWidget {
+  // TodayRecipeListView needs a list of recipes to display.
   final List<ExploreRecipe> recipes;
   const TodayRecipeListView({Key? key, required this.recipes})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
+    // Within build(), start by applying some padding.
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+      // Add a Column to place widgets in a vertical layout
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+// In the column, add a Text. This is the header for the Recipes of the Day
           Text('Recipes of the Day " ',
               style: Theme.of(context).textTheme.headline1),
+          // Add a 16-point-tall SizedBox, to supply some padding
           const SizedBox(height: 16),
           // 7
           Container(
@@ -26,4 +33,5 @@ class TodayRecipeListView extends StatelessWidget {
       ),
     );
   }
+  // TODO: Add buildCard() widget here
 }
