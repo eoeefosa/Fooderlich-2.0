c44b91626d09ec604e423f9dc641293bbf5f6c4e
eoeefosa <eoeefosa@gmail.com>
2022-08-01 15:05:40 +0100

    added comment to friends_post_title.dart

--- lib/components/friends_post_title.dart
+++ lib/components/friends_post_title.dart
@@ -13,16 +13,24 @@ class FriendsPostTitle extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
+        // The first element is a circular avatar, which displays the
+        // image asset associated with the post.
         CircleImage(
           imageProvider: AssetImage(post.profileImageUrl),
           imageRadius: 20,
         ),
+        // Apply a 16-point padding
         const SizedBox(width: 16),
+        // Create Expanded, which makes the children fill the rest
+        //of the container.
         Expanded(
+            // Establish a Column to arrange the widgets vertically
             child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Create a Text to display your friend’s comments.
             Text(post.comment),
+            // Create another Text to display the timestamp of a post.
             Text(
               '${post.timestamp} mins ago',
               style: const TextStyle(fontWeight: FontWeight.w700),
